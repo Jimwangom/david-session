fe4c2e10bba783af8e65b29c0bc5dd4900e15d24
Jimwangom <jimymwangomi@gmail.com>
2025-01-01 19:04:48 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,20 +57,20 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
-┃SUCCESSFULLY
-┃CONNECTED ✅🔥
-┗━━━━━━━━━━━━━━━
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 DAVID CYRIL 𖥘
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+╔═══════════════════════
+║JAMES-MD V1 SESSION IS 
+║UCCESSFULLY
+║CONNECTED ⇨⇨⇨
+╚═══════════════════════
+◥◣◣◥◣◣◥◣◥◣◥◣◥◣◥◣◥◣◥◣◥
+❶ || 𝐜𝐫𝐞𝐚𝐭𝐨𝐫 = 𝐉𝐀𝐌𝐄𝐒
+◥◣◣◥◣◣◥◣◥◣◥◣◥◣◥◣◥◣◥◣◥
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vanxv5pB4hdUQlTg7Q2J
+◥◣◣◥◣◣◥◣◥◣◥◣◥◣◥◣◥◣◥◣◥
 Please Follow My Support Channel
-Wanna talk to me?👉 https://t.me/deecee_x 👈
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-2099 David Cyril*
+Wanna talk to me  = https://wa.me/255628470507
+◥◣◣◥◣◣◥◣◥◣◥◣◥◣◥◣◥◣◥◣◥
+2024-2099 ᴶᴬᴹᴱˢ
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
